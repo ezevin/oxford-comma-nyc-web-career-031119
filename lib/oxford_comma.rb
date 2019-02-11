ca564275335283e148e2e31b4ee2fcfaa50e71e0
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 22:48:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  array.join
-  if array.length == 2
+  if array.length <=2
+    return array.join
+     
+  elsif array.length == 2
     return array.join(" and ")
 
   elsif array.length == 3
