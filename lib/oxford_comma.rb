220047f7057448171726fc3849608c42d0516fab
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 22:49:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
      
   elsif array.length == 2
-    return array.prepend("and")join(" , ")
+    return array.prepend("and").join(" , ")
 
   elsif array.length == 3
     return array.join(" and ")
