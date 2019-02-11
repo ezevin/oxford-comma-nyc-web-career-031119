cba7b43ce342b4c1d7920c90986ed2d074c89ea2
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 22:40:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
  "Tardis, Serenity, Enterprise".split(",")
   if array.length == 2
     return array.join(" and ")
-  else if array.length
+  else if array.length == 3
+else if array.length > 3
 end
 
 
