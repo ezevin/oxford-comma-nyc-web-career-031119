67dbeaea88028a1b0c64fd2e8e7e139acf970468
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 22:57:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     return array.join(" and ")
 
   elsif array.length == 3
-  return array.last.insert("and").join(" , ")
+  return array.last.insert(-1, "and").join(" , ")
     
 
   elsif array.length > 3
